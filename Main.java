7c60103e0664b31990b5dad9d05691f26d411234
Andrew Nithack <douglasnithack@gmail.com>
2022-05-07 13:56:47 -0300

    criando main

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    System.out.println("exercico quadrilatero");
+    Quadrilatero.area(3);
+    Quadrilatero.area(5, 5);
+    Quadrilatero.area(7, 8, 9);
+    Quadrilatero.area(5f, 5f);
+  }
+
+}
